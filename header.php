25477d8a186bc1d46c1d995ab7d5734322be153d
Severin Meier <sev.meier@gmail.com>
2017-09-04 21:09:59 +0200

    Now compatible with mobile devices

--- header.php
+++ header.php
@@ -6,6 +6,8 @@
         href="<?php bloginfo('stylesheet_url'); ?>"/>
     <link href="https://fonts.googleapis.com/css?family=Catamaran:200"
         rel="stylesheet">
+    <meta name="viewport" content="initial-scale=1.0" />
+    <meta charset="utf-8" />
     <?php wp_head(); ?>
 </head>
 <body>
