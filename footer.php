d14495420c8a68a35ed4a3e9182425626a89fa8d
Severin Meier <sev.meier@gmail.com>
2017-09-08 13:19:43 +0200

    added facebook logo

--- footer.php
+++ footer.php
@@ -1,6 +1,21 @@
 <?php
 /* The footer of the page.
  */
+
+include_once('php-utils/color-utils.php');
+
+function getFbLogoSource($size) {
+    $fb_logo_folder = get_template_directory_uri().
+        '/assets/images/f_logo_online/png/';
+    $theme_color = get_theme_mod('theme_color');
+    $wb_complement = wbComplementSelect($theme_color);
+    if ($wb_complement == '#000000') {
+        $fb_logo_color = 'blue';
+    } else {
+        $fb_logo_color = 'white';
+    }
+    return $fb_logo_folder.'FB-f-Logo__'.$fb_logo_color.'_'.$size.'.png';
+}
 ?>
 <footer class="theme-color-bg">
     <section id="footer-content" class="content-width">
@@ -10,7 +25,9 @@
             ?>
             <span>Besuche uns auf <a
                 href="<?php echo $facebook_page ?>"
-                target="_blank">Facebook</a>
+                target="_blank">
+                <img src="<?php echo getFbLogoSource(29); ?>" />
+            </a>
             </span>
             <?php
         }
